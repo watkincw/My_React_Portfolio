3edcfb995dc222ae1226c4de83445f21bb75f38d
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-07 17:37:17 -0500

    Work to be done; started integgrating components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import Nav from './components/Nav';
+import Header from './components/Header';
+import AboutMe from './components/About';
+import Footer from './components/Footer';
+import ContactForm from './components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [categories] = useState([
+        { name: 'about me', description: 'A little about me!' },
+        { name: 'portfolio', description: 'Some of my work' },
+        { name: 'contact', description: 'Contact me!' },
+        { name: 'resume', description: 'My Resume' }
+    ]);
+
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    const [contactSelected, setContactSelected] = useState(false);
+
+
+	return (
+		<div className="App">
+			<Header
+				categories={ categories }
+				setCurrentCategory={ setCurrentCategory }
+				currentCategory={ currentCategory }
+				contactSelected={ contactSelected }
+				setContactSelected={ setContactSelected }
+			></Header>
+			<main>
+                <div>
+                    { !contactSelected ? (
+                        <>
+                            <AboutMe currentCategory={ currentCategory }></AboutMe>
+                        </>
+                    ) : (
+                        <ContactForm></ContactForm>
+                    ) }
+                </div>
+            </main>
+			<Footer />
+        </div>
+	);
 }
 
 export default App;
