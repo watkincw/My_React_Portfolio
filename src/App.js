f4880253d984f2ecb409409614a4cf92e4cd29ce
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-07 17:58:09 -0500

    edited file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-// import Nav from './components/Nav';
-import Header from './components/Header';
+// import Nav from './components/common/Nav';
+import Header from './components/common/Header';
 import AboutMe from './components/About';
-import Footer from './components/Footer';
+import Footer from './components/common/Footer';
 import ContactForm from './components/Contact';
 
 function App() {
--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import {capitalizeFirstLetter } from '../../utils/helpers';
+import {capitalizeFirstLetter } from '../../../utils/helpers';
 
 function Header(props) {
     const {
