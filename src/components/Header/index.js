0f2e9cbca5b41d280feefd25a53d9d08d362d0aa
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-07 17:34:28 -0500

    created file and most of work done for Header comp

---
+++ src/components/Header/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import {capitalizeFirstLetter } from '../../utils/helpers';
+
+function Header(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+        contactSelected,
+        setContactSelected
+    } = props;
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+
+    return (
+        <header classname="flex-row px-1">
+            <h2>
+                <a href="/">Colton Watkins</a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about" onClick={ () => setContactSelected(false) }>About Me</a>
+                    </li>
+                    <li className={ `mx-2 ${contactSelected && 'nacActive'} `}>
+                        <span onClick={() => setContactSelected(true) }>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                            className={ `mx-1 ${
+                                currentCategory.name === category.name && !contactSelected && 'navActive'
+                                }`}
+                            key={ category.name }
+                        >
+                            <span 
+                                onClick={ () => {
+                                    setCurrentCategory(category);
+                                    setContactSelected(false);
+                                } }
+                            >
+                                { capitalizeFirstLetter(category.name) }
+                            </span>
+                        </li>
+                    )) }
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
