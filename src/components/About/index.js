9bb0e3712135c9e39ac50aa7a8a6aaf7141ff192
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-07 17:35:57 -0500

    create file;most of work done in AboutMe component

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import coverImage from '../../assets/images/me.jpg';
+
+function AboutMe() {
+    return (
+        <section className="my-5">
+            <h1 id="aboutMe">About Me</h1>
+            <img
+                src={ coverImage }
+                className="my-2"
+                style={ { width: "100%" } }
+                alt="Colton Watkins"
+            />
+            <div className="my-2">
+                <p>
+                    Full stack web developer educated at UW-Extension BootCamp. Skills in REACT, JavaScript, SQL, and more. Strengths in meeting deadlines, 
+                    creativity, and teamwork. Passionate about approaching programming challenges from different angles and collaborating with others to create 
+                    meaningful web applications. Excited to develop responsive websites. Positioned to provide unique perspectives on how end users interact with websites and 
+                    software platforms with a unique perspective.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default AboutMe;
