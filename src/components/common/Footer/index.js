8fa20f280cdf3a99b1eafcec31f014b57759a3cf
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-10 15:11:30 -0500

    img alts

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -8,9 +8,9 @@ function Footer() {
     return (
         <CFooter>
             <div className="flex-row" name="Links">
-                    <a href="https://github.com/watkincw"><img alt="" src="./icons/GitHub-Logos/GitHub_Logo.png" /></a>
-                    <a href="https://www.linkedin.com/in/colton-watkins-2059a8211/"><img alt="" src="./icons/LinkedIn-Logos/LI-Logo.png" /></a>
-                    <a href="https://stackoverflow.com/users/15890614/colton-watkins"><img alt="" src="./icons/StackOverflow/logo-stackoverflow.png" /></a>
+                    <a href="https://github.com/watkincw"><img alt="GitHub Link" src="./icons/GitHub-Logos/GitHub_Logo.png" /></a>
+                    <a href="https://www.linkedin.com/in/colton-watkins-2059a8211/"><img alt="LinkedIn Link" src="./icons/LinkedIn-Logos/LI-Logo.png" /></a>
+                    <a href="https://stackoverflow.com/users/15890614/colton-watkins"><img alt="StackOverflow Link" src="./icons/StackOverflow/logo-stackoverflow.png" /></a>
             </div>
             <div>
                 <CLink href="https://coreui.io">CoreUI</CLink>
